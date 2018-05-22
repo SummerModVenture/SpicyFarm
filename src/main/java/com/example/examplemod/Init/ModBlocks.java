a30630aecf8697143f80a3b441120cf4ec6ab05d
DEVIN ROONEY <devroo2000@gmail.com>
2018-05-22 17:01:22 -0400

    added forge files

---
+++ src/main/java/com/example/examplemod/Init/ModBlocks.java
@@ -0,0 +1,41 @@
+import com.example.examplemod.Blocks.BlockBasic;
+import com.example.examplemod.MainMod;
+import net.minecraft.block.Block;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraft.block.material.Material;
+
+@Mod.EventBusSubscriber(modid= MainMod.MODID)
+public class ModBlocks {
+
+    static Block tutorialBlock;
+
+    public static void init() {
+        tutorialBlock = new BlockBasic("tutorial_block", Material.ROCK);
+    }
+
+    @SubscribeEvent
+    public static void registerBlocks(RegistryEvent.Register<Block> event) {
+        event.getRegistry().registerAll(tutorialBlock);
+    }
+
+    @SubscribeEvent
+    public static void registerItemBlocks(RegistryEvent.Register<Item> event) {
+        event.getRegistry().registerAll(new ItemBlock(tutorialBlock).setRegistryName(tutorialBlock.getRegistryName()));
+    }
+
+    @SubscribeEvent
+    public static void registerRenders(ModelRegistryEvent event) {
+        registerRender(Item.getItemFromBlock(tutorialBlock));
+    }
+
+    public static void registerRender(Item item) {
+        ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation( item.getRegistryName(), "inventory"));
+    }
+}
