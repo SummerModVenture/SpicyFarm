357ea3fd4fce32f91a79cf374c176d1b123e7eeb
Devin Rooney <devroo2000@gmail.com>
2018-05-22 21:58:50 -0400

    added forge files

---
+++ src/main/java/com/SpicyFarm/farm/Reference.java
@@ -0,0 +1,8 @@
+package com.SpicyFarm.farm;
+
+public class Reference {
+    public static final String MODID = "SpicyFarm";
+    public static final String MODNAME = "Spicy Farm";
+    public static final String VERSION = "1.0";
+    public static final String ACCEPTED_MINECRAFT_VERSIONS = "[1.12]";
+}
